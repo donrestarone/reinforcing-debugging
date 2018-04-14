84835795165bc41fc44819380dfc76adf92090d1
donrestarone <shashikejayatunge@gmail.com>
2018-04-14 13:21:08 -0400

    dont know what is going on here, need guidance

--- select-cards.rb
+++ select-cards.rb
@@ -1,20 +1,35 @@
 def select_cards(possible_cards, hand)
-
+	obj = 0
   possible_cards.each do |current_card|
-    print "Do you want to pick up #{current_card}?"
-    answer = gets.chomp
-    if answer.downcase == 'y'
-      hand << current_card
-    end 
+    if obj < 3  
+    	print "Do you want to pick up #{current_card}?"
+    	answer = gets.chomp
+    	if answer.downcase == 'y'
+			hand << current_card
+      		obj += 1 
+		else answer.downcase == 'n'
+		obj += 1 
+		puts obj
+		puts "Do you want to pick up #{possible_cards[obj]}?"
+		answer = gets.chomp
+		end
+
+	else obj < 3
+		select_cards(possible_cards, hand)
+	end 
     return hand
   end
 
 end
 
+
+
+
 available_cards = ['queen of spades', '2 of clubs', '3 of diamonds', 'jack of spades', 'queen of hearts']
 
 new_hand = select_cards(available_cards, [])
 
-display_hand = new_hand.join("\n")
+display_hand = new_hand.join('\n')
 
 puts "Your new hand is: \n#{display_hand}"
+
