4c89eaac49012c04ebf2a2969a327c68b080ff33
donrestarone <shashikejayatunge@gmail.com>
2018-04-16 14:38:40 -0400

    finish exercise

--- select-cards.rb
+++ select-cards.rb
@@ -7,18 +7,13 @@ def select_cards(possible_cards, hand)
     	if answer.downcase == 'y'
 			hand << current_card
       		obj += 1 
-		else answer.downcase == 'n'
-		obj += 1 
-		puts obj
-		puts "Do you want to pick up #{possible_cards[obj]}?"
-		answer = gets.chomp
+		 
 		end
 
-	else obj < 3
-		select_cards(possible_cards, hand)
+	
 	end 
-    return hand
   end
+    return hand
 
 end
 
@@ -29,7 +24,7 @@ available_cards = ['queen of spades', '2 of clubs', '3 of diamonds', 'jack of sp
 
 new_hand = select_cards(available_cards, [])
 
-display_hand = new_hand.join('\n')
+display_hand = new_hand.join("\n")
 
 puts "Your new hand is: \n#{display_hand}"
 
